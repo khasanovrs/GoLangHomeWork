cc8370ddcd152cd042733e564aa4b2222dc5dd6a
Khasanov Ruslan <khasanov--ruslan@yandex.ru>
2019-12-15 09:44:15 +0300

    feat: first practice

---
+++ hw1/practice/FmtPrintLn.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Руслан"
+	var surname = "Хасанов"
+	fmt.Println("Меня зовут", name, surname)
+
+	var age = 36
+	fmt.Println("Мне", age, "лет")
+}
