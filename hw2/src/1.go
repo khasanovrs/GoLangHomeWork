92a4cb2e4c308f06cc158fb3d2a5b3a9f51cdbde
Khasanov Ruslan <khasanov--ruslan@yandex.ru>
2019-12-16 16:18:28 +0300

    fix: hw2 task 1 fix function isNumberEven - change it to canDivide

--- hw2/src/1.go
+++ hw2/src/1.go
@@ -6,13 +6,13 @@ func main() {
 	var number int
 	fmt.Println("Укажите целое число:")
 	fmt.Scan(&number)
-	if isNumberEven(number) {
+	if canDivide(number, 2) {
 		fmt.Println("Данное число четное!")
 	} else {
 		fmt.Println("Данное число нечетное!")
 	}
 }
 
-func isNumberEven(number int) bool {
-	return number%2 == 0
+func canDivide(number int, divider int) bool {
+	return number%divider == 0
 }
